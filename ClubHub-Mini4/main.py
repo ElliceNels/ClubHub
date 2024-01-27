e1136d1176fef7757c3779cd0fd828a54d16eac5
ElliceNels <145558058+ElliceNels@users.noreply.github.com>
2024-01-27 22:03:25 +0000

    quick tests

--- ClubHub-Mini4/main.py
+++ ClubHub-Mini4/main.py
@@ -7,9 +7,24 @@ def hello_world():
     return """
     <html>
     <head>
+        <style>
+            h1 {
+            color: #996633;
+            text-align: center;
+            font-size: 28px;
+            }
+
+            p {
+            font-family: verdana;
+            font-size: 18px;
+            text-align: center;
+            }
+        </style>
     </head>
     <body>
         <h1>ClubHub Online</h1>
+        <p>This is the Offical webpage of ClubHub.<p>
+        <p>Rights of this website are accredited to 'Ellice', 'Michelle', 'Kelly', 'Alisia'.
     </body>
     </html>
     """
