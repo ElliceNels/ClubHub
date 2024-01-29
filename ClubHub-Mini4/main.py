bd622e7db1234e82d14344ee1d768018be8ef0ce
ElliceNels <145558058+ElliceNels@users.noreply.github.com>
2024-01-29 20:48:56 +0000

    reposition files

--- ClubHub-Mini4/main.py
+++ ClubHub-Mini4/main.py
@@ -8,7 +8,7 @@ def hello_world():
    
 <html>
 <head>
-<title>W3.CSS Template</title>
+<title>ClubHub</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
