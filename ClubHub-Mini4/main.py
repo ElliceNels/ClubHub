7411edb45456ee9ad4260b86996598284f0e8b4a
ElliceNels <145558058+ElliceNels@users.noreply.github.com>
2024-01-30 10:05:19 +0000

    css changes

--- ClubHub-Mini4/main.py
+++ ClubHub-Mini4/main.py
@@ -3,9 +3,11 @@ from flask import Flask, render_template
 # Provide template folder name
 app = Flask(__name__, template_folder='templateFiles', static_folder='staticFiles')
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
