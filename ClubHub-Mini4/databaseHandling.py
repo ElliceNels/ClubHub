dcaa62baa0b5e455e0e5d08465d04d7daa4d2ce6
Elle0-0 <andreamichellevaz@gmail.com>
2024-01-30 10:01:09 +0000

    coordinator tables added

--- ClubHub-Mini4/databaseHandling.py
+++ ClubHub-Mini4/databaseHandling.py
@@ -1,6 +1,7 @@
 import sqlite3
 conn = sqlite3.connect('Clubhub.db')
 print('Database connected')
+cursor = conn.cursor()
 
 sql = """ 
 -- tables
@@ -21,6 +22,25 @@ CREATE TABLE EVENT_ATTENDEES (
     PRIMARY KEY (Uid, Eid)
 );
 
+CREATE TABLE IF NOT EXISTS CLUBS (
+               Cid INTEGER PRIMARY KEY,
+               Club_name VARCHAR(40),
+               Uid INTEGER,
+               Description TEXT,
+               Validity_status VARCHAR(20),
+               Created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+               Updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+               FOREIGN KEY (Uid) REFERENCES USER_LOGIN(Uid)
+        );
+
+
+CREATE TABLE IF NOT EXISTS USER_TYPE (
+               Uid INTEGER PRIMARY KEY,
+               Type VARCHAR(20),
+               Created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+               Updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
+        );
+        
 --triggers
 
 CREATE TRIGGER updated_trigger_club_memberships AFTER UPDATE ON CLUB_MEMBERSHIP
@@ -36,6 +56,23 @@ BEGIN
     SET Updated = CURRENT_TIMESTAMP
     WHERE Uid = OLD.Uid AND Eid = OLD.Eid;
 END;
+
+ CREATE TRIGGER update_clubs AFTER UPDATE ON CLUB_LIST
+            BEGIN
+               UPDATE CLUB_LIST
+               SET Updated_at = CURRENT_TIMESTAMP
+               WHERE Cid = OLD.Cid;
+            END;
+        );
+
+CREATE TRIGGER update_user_type AFTER UPDATE ON USER_TYPE
+            BEGIN
+               UPDATE USER_TYPE
+               SET Updated_at = CURRENT_TIMESTAMP
+               WHERE Uid = OLD.Uid;
+            END;
 """
 
-cursor = conn.cursor()
+
+cursor.close()
+conn.close()
