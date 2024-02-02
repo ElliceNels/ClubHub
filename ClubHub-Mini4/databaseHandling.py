d3d8b8f6436823ffbab1ea771b63fea2f2eb1196
Kabidoye-17 <23365412@studentmail.ul.ie>
2024-02-02 12:58:07 +0000

    Seperate out sql table schema

--- ClubHub-Mini4/databaseHandling.py
+++ ClubHub-Mini4/databaseHandling.py
@@ -3,10 +3,8 @@ conn = sqlite3.connect('Clubhub.db')
 print('Database connected')
 cursor = conn.cursor()
 
-sql = """ 
--- tables
-
-CREATE TABLE IF NOT EXISTS CLUB_MEMBERSHIP (
+# tables
+cursor.execute('''CREATE TABLE IF NOT EXISTS CLUB_MEMBERSHIP (
     Membership_id INTEGER PRIMARY KEY AUTOINCREMENT,
     User_id INTEGER,
     Club_id INTEGER,
@@ -16,8 +14,10 @@ CREATE TABLE IF NOT EXISTS CLUB_MEMBERSHIP (
     Updated DATETIME DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (User_id) REFERENCES USER_LOGIN(User_id)
     FOREIGN KEY (Club_id) REFERENCES CLUBS(Club_id)
-);
+);''')
+conn.commit()
 
+cursor.execute('''
 CREATE TABLE IF NOT EXISTS EVENT_ATTENDEES (
     Attendee_id INTEGER PRIMARY KEY AUTOINCREMENT,
     User_id INTEGER,
@@ -28,8 +28,10 @@ CREATE TABLE IF NOT EXISTS EVENT_ATTENDEES (
     Updated DATETIME DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (User_id) REFERENCES USER_LOGIN(User_id)
     FOREIGN KEY (Event_id) REFERENCES EVENTS(Event_id)
-);
+); ''')
+conn.commit()
 
+cursor.execute('''
 CREATE TABLE IF NOT EXISTS CLUBS (
     Cid INTEGER PRIMARY KEY,
     Club_name VARCHAR(40),
@@ -39,16 +41,19 @@ CREATE TABLE IF NOT EXISTS CLUBS (
     Created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     Updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (Uid) REFERENCES USER_LOGIN(Uid)
-);
+);''')
+conn.commit()
 
-
+cursor.execute('''
 CREATE TABLE IF NOT EXISTS USER_TYPE (
     Uid INTEGER PRIMARY KEY,
     Type VARCHAR(20),
     Created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     Updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
-);
-        
+);''')
+conn.commit()
+
+cursor.execute('''
 CREATE TABLE IF NOT EXISTS USER_LOGIN(
     Uid INTEGER (8),
     Firstname VARCHAR (50),
@@ -58,8 +63,10 @@ CREATE TABLE IF NOT EXISTS USER_LOGIN(
     Updated DATETIME DEFAULT CURRENT_TIMESTAMP,
     Created DATETIME DEFAULT CURRENT_TIMESTAMP, 
     CONSTRAINT ULPK PRIMARY KEY (Uid)
-);
+); ''')
+conn.commit()
  
+cursor.execute('''
 CREATE TABLE IF NOT EXISTS USER_DETAILS(
   Uid INTEGER (8),
   Username VARCHAR(50),
@@ -68,7 +75,10 @@ CREATE TABLE IF NOT EXISTS USER_DETAILS(
   Created DATETIME DEFAULT CURRENT_TIMESTAMP,
   CONSTRAINT UDPK PRIMARY KEY (Uid),
   CONSTRAINT UDFK FOREIGN KEY (Uid) REFERENCES USER_LOGIN (Uid)
-);
+); ''')
+conn.commit()
+
+cursor.execute('''
 
 CREATE TABLE IF NOT EXISTS EVENTS(
   Eid INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -81,61 +91,77 @@ CREATE TABLE IF NOT EXISTS EVENTS(
   Updated DATETIME DEFAULT CURRENT_TIMESTAMP,
   Created DATETIME DEFAULT CURRENT_TIMESTAMP,
   FOREIGN Key(Cid) REFERENCES CLUBS(Cid)
-);
+);''')
+conn.commit()
 
 
---triggers
+##triggers
 
+cursor.execute('''
 CREATE TRIGGER updateevents AFTER UPDATE ON EVENTS
 BEGIN 
     UPDATE EVENTS
     SET Updated = CURRENT_TIMESTAMP
     WHERE Eid = OLD.Eid;
-    END;
+    END;''')
+conn.commit()
 
+cursor.execute('''
 CREATE TRIGGER updatelogin AFTER UPDATE ON USER_LOGIN
 BEGIN
     UPDATE USER_LOGIN
     SET Updated = CURRENT_TIMESTAMP
     WHERE Uid = OLD.Uid;
-END;
+END; ''')
+conn.commit()
 
-
+cursor.execute('''
 CREATE TRIGGER updatedetails AFTER UPDATE ON USER_DETAILS
 BEGIN
     UPDATE USER_DETAILS
     SET Updated = CURRENT_TIMESTAMP
     WHERE Uid = OLD.Uid;
-END;
+END; ''')
+conn.commit()
+
+cursor.execute('''
 
 CREATE TRIGGER update_clubs AFTER UPDATE ON CLUBS
 BEGIN
     UPDATE CLUB_LIST
     SET Updated_at = CURRENT_TIMESTAMP
     WHERE Cid = OLD.Cid;
-END;
+END; ''')
+conn.commit()
 
+cursor.execute('''
 CREATE TRIGGER update_user_type AFTER UPDATE ON USER_TYPE
 BEGIN
     UPDATE USER_TYPE
     SET Updated_at = CURRENT_TIMESTAMP
     WHERE Uid = OLD.Uid;
-END;
+END; ''')
+conn.commit()
+
+cursor.execute('''
             
 CREATE TRIGGER updated_trigger_club_memberships AFTER UPDATE ON CLUB_MEMBERSHIP
 BEGIN
     UPDATE CLUB_MEMBERSHIP
     SET Updated = CURRENT_TIMESTAMP
     WHERE User_id = OLD.User_id AND Club_id = OLD.Club_id;
-END;
+END; ''')
+conn.commit()
 
+cursor.execute('''
 CREATE TRIGGER updated_trigger_event_attendees AFTER UPDATE ON EVENT_ATTENDEES
 BEGIN
     UPDATE EVENT_ATTENDEES
     SET Updated = CURRENT_TIMESTAMP
     WHERE User_id = OLD.User_id AND Event_id = OLD.Event_id;
-END;
-"""
+END; ''')
+conn.commit()
+
 
 cursor.close()
 conn.close()
