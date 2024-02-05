8d390f821cf86013c1ae58cf2af05dcb88ab25a1
Elle0-0 <andreamichellevaz@gmail.com>
2024-02-05 14:18:32 +0000

    html for clubs display

--- ClubHub-Mini4/main.py
+++ ClubHub-Mini4/main.py
@@ -4,9 +4,16 @@ from flask import Flask, render_template
 app = Flask(__name__, template_folder='TemplateFiles', static_folder='StaticFiles')
 
 
+clubs = [
+    {"name": "Club 1", "description": "Description for Club 1", "coordinator_name": "John Doe"},
+    {"name": "Club 2", "description": "Description for Club 2", "coordinator_name": "Jane Doe"},
+    {"name": "Club 3", "description": "Description for Club 3", "coordinator_name": "Bob Smith"},
+]
+
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('clubs_display.html', clubs=clubs)
 
 
 if __name__ == '__main__':
