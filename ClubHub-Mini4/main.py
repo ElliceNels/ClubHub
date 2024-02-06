bc0ed96e952960a7f8970ab706543176e014c247
ElliceNels <145558058+ElliceNels@users.noreply.github.com>
2024-02-06 12:51:22 +0000

    inbox with navbar working

--- ClubHub-Mini4/main.py
+++ ClubHub-Mini4/main.py
@@ -1,12 +1,23 @@
 from flask import Flask, render_template
 
 # Provide template folder name
-app = Flask(__name__, template_folder='TemplateFiles', static_folder='StaticFiles')
+app = Flask(__name__, template_folder='templateFiles', static_folder='staticFiles')
 
 
 @app.route('/')
+@app.route('/Inbox')
 def index():
-    return render_template('index.html')
+    return render_template('Inbox.html')
+
+
+@app.route("/Clubs")
+def Clubs():
+    return render_template('Clubs.html')
+
+
+@app.route("/Events")
+def Events():
+    return render_template('Events.html')
 
 
 if __name__ == '__main__':
