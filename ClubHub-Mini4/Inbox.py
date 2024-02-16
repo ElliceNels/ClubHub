2992d629fc74474a6b1dff96e93db639b7b979dd
ElliceNels <145558058+ElliceNels@users.noreply.github.com>
2024-02-16 14:48:08 +0000

    Event and club requests work

--- ClubHub-Mini4/Inbox.py
+++ ClubHub-Mini4/Inbox.py
@@ -1,7 +1,18 @@
 import sqlite3
 from constants import DB_PATH
-
+from itertools import chain
 from Verification import Verification
+
+
+def isMemberOfClub(User_id):
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+
+    cursor.execute('''SELECT * FROM CLUB_MEMBERSHIP WHERE User_id = ?''', (User_id,))
+    if cursor.fetchall():
+        return True
+    else:
+        return False
 
 
 class Inbox:
@@ -10,47 +21,46 @@ class Inbox:
         self.userList = []
         self.waitingList = []
 
-    def isMemberOfClub(self, User_id):
-        conn = sqlite3.connect(DB_PATH)
-        cursor = conn.cursor()
-
-        cursor.execute('''SELECT * FROM CLUB_MEMBERSHIP WHERE User_id = ?''', (User_id,))
-        if cursor.fetchall():
-            return True
-        else:
-            return False
-
-
     def CoordIDtoEventsID(self, CoordId):
         conn = sqlite3.connect(DB_PATH)
         cursor = conn.cursor()
+        print("Coord ID To Event ID:", CoordId, )
         ClubId = self.CoordIDtoClubID(CoordId)
         EventId = cursor.execute('''SELECT Event_id FROM EVENTS WHERE Club_id = ?''', (ClubId,))
 
         Events = EventId.fetchall()
-        return Events
 
-    def CoordIDtoClubID(CoordId):
+        ids = []
+        for e in Events:
+            ids.append(e[0])
+
+        return ids
+
+    def CoordIDtoClubID(self, CoordId):
         conn = sqlite3.connect(DB_PATH)
         cursor = conn.cursor()
 
+        print("Get Club id for coordinator:", CoordId)
         ClubId = cursor.execute('''SELECT Club_id FROM CLUBS WHERE Coordinator_id = ?''', (CoordId,))
         id = ClubId.fetchone()
-        return id
+
+        if id is None:
+            return None
+        return id[0]
 
     def clubApprovalList(self, User_id, pendingstatus):
         conn = sqlite3.connect(DB_PATH)
         cursor = conn.cursor()
 
         CoordID = Verification.UserIdToCoordId(User_id)
-        club_id = Inbox.CoordIDtoClubID(CoordID)
-        for Club_id in club_id:
-            print('club id is' , Club_id)
+        club_id = self.CoordIDtoClubID(CoordID)
+        print('club id is', club_id)
 
-            code = cursor.execute(''' SELECT cm.User_id, Firstname, Lastname FROM CLUB_MEMBERSHIP cm INNER JOIN USER_DETAILS ud ON cm.User_id = ud.User_id WHERE cm.Is_pending = ? AND cm.Club_id = ?''',
-                (pendingstatus, Club_id))
-            print(code)
-            self.waitingList = [list(row) for row in code.fetchall()]
+        code = cursor.execute(
+            ''' SELECT cm.User_id, Firstname, Lastname FROM CLUB_MEMBERSHIP cm INNER JOIN USER_DETAILS ud ON cm.User_id = ud.User_id WHERE cm.Is_pending = ? AND cm.Club_id = ?''',
+            (pendingstatus, club_id))
+        print(code)
+        self.waitingList = [list(row) for row in code.fetchall()]
 
         for user in self.waitingList:
             cursor.execute(''' SELECT User_id FROM COORDINATORS Where User_id = ?''', (int(user[0]),))
@@ -61,8 +71,7 @@ class Inbox:
 
         return self.waitingList
 
-
-    def getUserList(self, User_id, pendingstatus, approvedstatus):
+    def getEventWaitList(self, User_id, pendingstatus):
         conn = sqlite3.connect(DB_PATH)
 
         cursor = conn.cursor()
@@ -70,23 +79,27 @@ class Inbox:
         # cursor.execute('''INSERT INTO CLUB_MEMBERSHIP (User_id, Club_id) VALUES(?,?)''', (4121234, 3))
         # conn.commit()
         CoordID = Verification.UserIdToCoordId(User_id)
-        Club_id = self.CoordIDtoClubID(CoordID)
-        Events = self.ClubIDtoEventID(Club_id)
+        Events = self.CoordIDtoEventsID(CoordID)
+
+        print('Coordid', CoordID)
+        print('event id', Events)
+        ids = ", ".join([str(id) for id in Events])
+        print(ids)
 
         cursor.execute(
-            ''' SELECT ea.User_id, Firstname, Lastname FROM EVENT_ATTENDEES ea INNER JOIN USER_DETAILS ud ON ea.User_id = ud.User_id WHERE ea.Is_pending = ? AND ea.Event_id = ?''',
-            (pendingstatus, Events))
+            f''' SELECT ea.User_id, Firstname, Lastname FROM EVENT_ATTENDEES ea INNER JOIN USER_DETAILS ud ON ea.User_id = ud.User_id WHERE ea.Is_pending = ? AND ea.Event_id IN ({ids})''',
+            (pendingstatus,))
         self.eventList = [list(row) for row in cursor.fetchall()]
         print(self.eventList)
         for user in self.eventList:
             cursor.execute(''' SELECT User_id FROM COORDINATORS Where User_id = ?''', (int(user[0]),))
             user.append(user[1] + " would like to come to your event")
 
+
         cursor.close()
         conn.close()
 
         return self.eventList
-
 
     def individualapproveOrReject(self, User_id, status):
         conn = sqlite3.connect(DB_PATH)
@@ -133,3 +146,7 @@ class Inbox:
             finally:
                 cursor.close()
                 conn.close()
+
+
+NNN = Inbox()
+NNN.getEventWaitList(4121212, 1)
