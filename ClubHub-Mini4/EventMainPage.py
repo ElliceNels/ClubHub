de329c0adfca6ef96209eabadf7cea0a7eb8a7fa
alisiak03 <145369992+alisiak03@users.noreply.github.com>
2024-02-18 20:44:08 +0000

    added description to events

--- ClubHub-Mini4/EventMainPage.py
+++ ClubHub-Mini4/EventMainPage.py
@@ -6,13 +6,13 @@ import sqlite3
 def eventsmainpage():
     connection = sqlite3.connect(DB_PATH)
     cursor = connection.cursor()
-    cursor.execute('SELECT EventTitle, EventTime, EventDate FROM EVENTS')
+    cursor.execute('SELECT EventTitle, EventTime, Description, EventDate FROM EVENTS')
     eventsmainpage = cursor.fetchall()
 
 #coverting date strings to date objects since its being retrived as a string 
-    eventsmainpage = [(title, time, datetime.datetime.strptime(date_str, '%Y-%m-%d').date().strftime('%a, %b %d')) for title, time, date_str in eventsmainpage]
+    eventsmainpage = [(title, time, description, datetime.datetime.strptime(date_str, '%Y-%m-%d').date().strftime('%a, %b %d')) for title, time,description ,date_str in eventsmainpage]
     
-    eventsmainpage.sort(key=lambda x: x[2])
+    eventsmainpage.sort(key=lambda x: x[3])
     #this sorts events in ascending order based on the third item which is the date, key function extracts the date from each tuple
 #lambda creates an anoynmous function
     
