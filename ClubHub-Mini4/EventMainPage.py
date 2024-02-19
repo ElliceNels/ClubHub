d9c6de5f880b00225e83dfe38732fb031a781940
alisiak03 <145369992+alisiak03@users.noreply.github.com>
2024-02-19 09:03:14 +0000

    linked to details page

--- ClubHub-Mini4/EventMainPage.py
+++ ClubHub-Mini4/EventMainPage.py
@@ -12,9 +12,10 @@ def eventsmainpage():
 #coverting date strings to date objects since its being retrived as a string 
     eventsmainpage = [(title, time, description, datetime.datetime.strptime(date_str, '%Y-%m-%d').date().strftime('%a, %b %d')) for title, time,description ,date_str in eventsmainpage]
     
-    eventsmainpage.sort(key=lambda x: x[3])
+    eventsmainpage.sort(key=lambda x: datetime.datetime.strptime(x[3], '%a, %b %d').date())
     #this sorts events in ascending order based on the third item which is the date, key function extracts the date from each tuple
 #lambda creates an anoynmous function
+    #date string is being turned into a datetime object to parse the string - makes it easier to sort 
     
     cursor.close()
     connection.close()
