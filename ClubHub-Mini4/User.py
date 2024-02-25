2d1aff8792c3f9b81a68d080f7af6e35b0b129f4
Kabidoye-17 <23365412@studentmail.ul.ie>
2024-02-25 01:48:21 +0000

    code cleaned up and refactored

--- ClubHub-Mini4/User.py
+++ ClubHub-Mini4/User.py
@@ -4,16 +4,11 @@ class User:
     
     def update_user_information(self, table, column, new_value, user_id):
         conn = sqlite3.connect(DB_PATH)
-        cursor = conn.cursor()
-        
         try:
             with conn:
+                cursor = conn.cursor()
                 update_query = f''' UPDATE {table} SET {column} = ? WHERE User_id = ?'''
                 cursor.execute(update_query, ( new_value, user_id) )
                 conn.commit()
         except Exception as e:
             print(e)
-        finally:
-            cursor.close()
-            conn.close()
-        
